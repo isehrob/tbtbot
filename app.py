18911cd4dc797ac7b25c24ad9e954aec14c3a08a
Сехроб Ибрагимов <s_ibrohimov@translator.uzinfocom.uz>
2016-12-27 09:01:35 +0500

    improvments

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 import json
 
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
+
+
+# our bot token
+# sehrob_bot
+BOT_TOKEN = "224642244:AAHBHgcyUXyJyjYOaUbORf64k4kKAqcXv2U"
+# use like this: API % method_name
+API = "https://api.telegram.org/bot%s/%s" % (BOT_TOKEN, "%s")
+
+# client = httpclient.HTTPClient()
+client = tornado.httpclient.AsyncHTTPClient()
 
 
 class TelegramRequestHandler(tornado.web.RequestHandler):
     def __init__(self, *args, **kwargs):
         super(TelegramRequestHandler, self).__init__(*args, **kwargs)
-        self.message = json.loads(self.request.body.decode())
+        self.message = None
+        if self.request.body:
+            self.message = json.loads(self.request.body.decode())
+
+
+class WebHookHandler(TelegramRequestHandler):
+    def post(self):
+        print(self.message)
+        self.write("kuku")
+
+    def get(self):
+        self.post()
+
+
+class WebHookInfo(tornado.web.RequestHandler):
+    def get(self):
+        pass
 
 class MainHandler(TelegramRequestHandler):
     def get(self):
@@ -31,6 +59,7 @@ class QuestionHandler(TelegramRequestHandler):
 
 def make_app():
     return tornado.web.Application([
+        (r"/webhook", WebHookHandler),
         (r"/name", NameHandler),
         (r"/ask", QuestionHandler),
         (r"/.*", MainHandler),
@@ -39,5 +68,10 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8787, '0.0.0.0')
+    # app.listen(8787, '0.0.0.0')
+    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
+        "certfile": "/home/s_ibrohimov/projects/tornado/ssl/tbotprkey.pem",
+        "keyfile": "/home/s_ibrohimov/projects/tornado/ssl/tbotprkey.key"
+    })
+    http_server.listen(8443, '0.0.0.0')
     tornado.ioloop.IOLoop.current().start()
