0ee61cb3794af68802f26f4b87dcae8d1ec2f59f
Сехроб Ибрагимов <s_ibrohimov@translator.uzinfocom.uz>
2016-12-28 15:15:23 +0500

    packaging the app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='tbtbot',
+    version='0.1dev',
+    packages=['tbtbot',],
+    license='MIT license',
+    long_description=open('README.md').read()
+)
+
