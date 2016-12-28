96ca3e8679c89675455cb1fbd5d57c15b7beb05e
Сехроб Ибрагимов <s_ibrohimov@translator.uzinfocom.uz>
2016-12-28 16:05:41 +0500

    packaging again

--- setup.py
+++ setup.py
@@ -3,8 +3,18 @@ from distutils.core import setup
 setup(
     name='tbtbot',
     version='0.1dev',
-    packages=['tbtbot',],
-    license='MIT license',
-    long_description=open('README.md').read()
+    author='Sehrob Ibrohimov'
+    packages=['tbtbot', 'tbtbot.test'],
+    license='License.txt',
+    description='Telegram bot'
+    long_description=open('README.md').read(),
+    install_requires=[
+        "envparse >= 0.2.0",
+        "tornado==4.4.2",
+    ],
+    entry_points={
+        'console_scripts':
+            ['tbtboter = tbtbot.scripts.tbtboter:main',]
+    },
 )
 
