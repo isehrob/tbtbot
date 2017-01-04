3ce8c47d28b12a87fab15f3bad2b518d17bec0cc
Сехроб Ибрагимов <s_ibrohimov@translator.uzinfocom.uz>
2017-01-04 15:37:21 +0500

    complete first release

--- tbtbot/app.py
+++ tbtbot/app.py
@@ -1,10 +1,13 @@
 import json
+import pickle
 
+import tornado
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.httpclient
 import tornado.gen
+import tornado.httputil
 
 from envparse import env
 
@@ -18,94 +21,100 @@ BOT_TOKEN = env('BOT_TOKEN')
 # use like this: API % method_name
 API = "https://api.telegram.org/bot%s/%s" % (BOT_TOKEN, "%s")
 
-# client = httpclient.HTTPClient()
-client = tornado.httpclient.AsyncHTTPClient()
+
+class _CustomRequestDispatcher(tornado.web._RequestDispatcher):
+
+    def finish(self):
+        # kind of a hack of tornado routing 
+        # here if we get some update through our
+        # webhook then we extract the command from
+        # the telegram update and reroute the request
+        if self.stream_request_body:
+            self.request.body.set_result(None)
+        else:
+            self.request.body = b''.join(self.chunks)
+            self.request._parse_body()
+
+        if self.request.path == '/webhook':
+            text = json.loads(
+                self.request.body.decode()
+            )['message']['text']
+            command = text if text.startswith('/') else '/main'
+            self.request.path = command
+            self._find_handler()
+
+        self.execute()
 
 
+# implementing our above mentioned hack
+class CustomApplication(tornado.web.Application):
+
+    def start_request(self, server_conn, request_conn):
+        return _CustomRequestDispatcher(self, request_conn)
+
+
+# Custom `RequestHandler` class which encapsulates the 
+# common functionality for telegram requests
 class TelegramRequestHandler(tornado.web.RequestHandler):
+
     def __init__(self, *args, **kwargs):
         super(TelegramRequestHandler, self).__init__(*args, **kwargs)
         self.message = None
+        self.update = None
+        # client = httpclient.HTTPClient()
+        self.client = tornado.httpclient.HTTPClient()
+
         if self.request.body:
-            self.message = json.loads(self.request.body.decode())
+            self.update = json.loads(self.request.body.decode())
 
+    def send(self):
+        self.client.fetch(API % 'sendMessage?chat_id=%s&text=%s'
+            % (self.update['message']['from']['id'], self.message))
 
-class WebHookHandler(TelegramRequestHandler):
-
-    @tornado.gen.coroutine
-    def post(self):
-        if self.message:
-            text = self.message['message']['text']
-            command = text if text.startswith('/') else '/main'
-            print('yes message', command)
-            self.redirect(command)
-        else:
-            print('no message')
-            self.redirect("/main")
-        # request = tornado.httpclient.HTTPRequest(
-        #     url=('https://91.212.89.6:8443%s' % command),
-        #     allow_nonstandard_methods=True,
-        #     body=json.dumps(self.message)
-        # )
-        # response = yield client.fetch(request)
-        # yield client.fetch(API % 'sendMessage?chat_id=%s&text=%s'
-        #     % (self.message['message']['from']['id'], response.body.decode()))
-    
-    def get(self):
-        self.post()
-
-
-class WebHookInfo(tornado.web.RequestHandler):
-    def get(self):
-        pass
 
 class MainHandler(TelegramRequestHandler):
-    def get(self):
-        print('/main', self.message)
-        self.redirect("/name")
+    def post(self):
+        self.message = "Hello!"
+        self.send()
 
 
 class NameHandler(TelegramRequestHandler):
-    def get(self):
-        name = "Juju"
-        print('/name', self.message)
-        self.write("My name is %s" % name)
+    def post(self):
+        self.message = "My name is this!"
+        self.send()  
 
 
 class QuestionHandler(TelegramRequestHandler):
-    def get(self):
-        answer = "I don't know"
-        print('/ask', self.message)
-        self.write(answer)
+    def post(self):
+        self.message = "I don't know!"
+        self.send()
 
 
 def make_app():
-    app = tornado.web.Application([
-        (r"/webhook", WebHookHandler),
+    app = CustomApplication([
+        (r"/webhook", tornado.web.RequestHandler),
         (r"/name", NameHandler),
         (r"/ask", QuestionHandler),
         (r"/.*", MainHandler),
     ])
 
-    #app.add_handlers(r"https://91.212.89.6",[
-     #   (r"/name", NameHandler),
-      #  (r"/ask", QuestionHandler),
-       # (r"/.*", MainHandler),
-    #])
-
     return app
 
 
-def kuku():
-    with open('check.txt', 'a') as f:
-        f.write('working-ku\n')
+# def kuku():
+#     # some kind of parallel task
+#     with open('check.txt', 'a') as f:
+#         f.write('working-ku\n')
+
 
 if __name__ == "__main__":
+
+    # pcb = tornado.ioloop.PeriodicCallback(kuku, 2000)
+    # pcb.start()
+
     http_server = tornado.httpserver.HTTPServer(make_app(), ssl_options={
         "certfile": env('CERTFILE'),
         "keyfile": env("KEYFILE")
     })
     http_server.listen(env('SERVER_PORT'), env('SERVER_HOST'))
-    pcb = tornado.ioloop.PeriodicCallback(kuku, 2000)
-    pcb.start()
     tornado.ioloop.IOLoop.current().start()
